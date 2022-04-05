9a7189c7d90554ba1e4d792d3fb7c06b44720122
lso5507 <leeseokwoon@naver.com>
2022-04-05 21:34:08 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-REPOSITORY = /home/ec2-user/app/deploy
-PROJECT_NAME = springboot2-webservice
+REPOSITORY=/home/ec2-user/app/deploy
+PROJECT_NAME=springboot2-webservice
 
 
 
@@ -7,12 +7,12 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 # 실행 중이면 종료하기 위해서 현재 수행 중인 프로세스id를 찾습니다.
 # springboot2-webservice으로 된 다른 프로그램들이 있을 수 있어 springboot2-webservice된 jar 프로세스를 찾은 뒤 id를 찾습니다(awk '{print $1}').
-CURRENT_PID = $(ps -ef | grep url-enc*)
+CURRENT_PID=$(ps -ef | grep url-enc*)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  ehco "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,9 +21,9 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME = $(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR name: JAR_NAME"
+echo "> JAR name: $JAR_NAME"
 echo "> $JAR_NAME에 실행 권한 추가"
 chmod +x $JAR_NAME
 
